dda9a6302a12d68d1890462991abfb68c8b9fa64
Blue-Sky-r <robert.puskajler@yahoo.ca>
2020-05-06 08:25:41 +0200

    cron wrapper

---
+++ cron-sunset.sh
@@ -0,0 +1,49 @@
+:
+
+# sunset script - trigger action at sunset + offset
+#
+# if the offset is negative = trigger the action offset minutes before sunset (like switch on the lights)
+#
+# REQUIRES: sunrise-sunset.awk
+#
+# USAGE: cron-sunset.sh [action pars]
+#
+# intended to be called from cron
+# it sleeps till $OFS minutes before sunset
+# and then executes the $ACTION
+#
+# https://github.com/blue-sky-r/sunrise-sunset
+
+# geolocation coordinates (BB.SK.EU)
+#
+LAT=48.736277
+LON=19.1461917
+
+# activation offset in minutes (negative = before sunset)
+#
+OFS=-10
+
+# syslog tag (empty to disable syslog)
+#
+TAG=sunset
+
+# executables dir
+#
+DIR="/www/cgi-bin"
+#DIR=$( pwd )
+
+# action (default 'relay.cgi on')
+#
+ACTION=${@:-relay.cgi on}
+
+# sleep until sunset - offset
+#
+$DIR/sunrise-sunset.awk -- $LAT $LON sunset $OFS sleep
+
+# optional syslog msg
+#
+[ -n "$TAG" ] && logger -t "$TAG" "ACTION - $ACTION"
+
+# execute action
+#
+$DIR/$ACTION
